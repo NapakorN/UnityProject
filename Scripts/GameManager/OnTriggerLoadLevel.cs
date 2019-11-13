e2d706b8fcadef890addc7569fbf7d778e4da8f2
NapakorN <53931723+NapakorN@users.noreply.github.com>
2019-11-13 18:01:25 +0700

    Add files via upload

---
+++ Scripts/GameManager/OnTriggerLoadLevel.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class OnTriggerLoadLevel : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject gameObject;
+
+    public string Leveltoload;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.name == "Player")
+        {
+            gameObject.SetActive(true);
+            if(gameObject && Input.GetKey("e"))
+            {
+                Application.LoadLevel(Leveltoload);
+            }
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        gameObject.SetActive(false);
+    }
+}
