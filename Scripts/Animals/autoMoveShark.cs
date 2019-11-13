bbcc11c2931fb0f50efcf538285af2108710f634
NapakorN <53931723+NapakorN@users.noreply.github.com>
2019-11-13 17:57:58 +0700

    Add files via upload

---
+++ Scripts/Animals/autoMoveShark.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class autoMoveShark : MonoBehaviour
+{
+    public Rigidbody SharkRigid;
+    private float SharkSpeed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SharkRigid = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SharkRigid.AddForce(0,0,-SharkSpeed*Time.deltaTime,ForceMode.VelocityChange);
+    }
+}
---
+++ Scripts/Animals/autoMoveShark4.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class autoMoveShark4 : MonoBehaviour
+{
+    public Rigidbody Shark;
+    private float SharkSpeed = 16f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Shark = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Shark.AddForce(0,0,-SharkSpeed*Time.deltaTime,ForceMode.VelocityChange);
+    }
+}
