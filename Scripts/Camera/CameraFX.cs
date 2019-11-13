959e6c08cfd7197d9e1dec54183722440f105f77
NapakorN <53931723+NapakorN@users.noreply.github.com>
2019-11-13 18:00:14 +0700

    Add files via upload

---
+++ Scripts/Camera/CameraFX.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PostProcessing;
+
+public class CameraFX : MonoBehaviour
+{
+    public PostProcessingProfile normal, waterFX;
+    private PostProcessingBehaviour CamFX;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CamFX = FindObjectOfType<PostProcessingBehaviour>();
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Player")
+        {
+            CamFX.profile = waterFX;
+        }
+    }
+    private void OnTriggerExit(Collider other) {
+        if (other.name == "Player")
+        {
+            CamFX.profile = normal;
+        }
+    }
+}
